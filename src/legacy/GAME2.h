536a8277cf63f5d9d8e82dac59bc36a63ef02d18
Jeong, Hun <blmarket@gmail.com>
2025-07-07 19:55:04 -0700

    Simple as bool

--- src/legacy/GAME2.h
+++ src/legacy/GAME2.h
@@ -56,7 +56,7 @@ int sub_451F90(struct576* a1);
 int sub_451FE0(struct576* a1);
 int sub_452010();
 void sub_452050(uint32_t* a1);
-int* sub_452120(struct576* a1);
+bool sub_452120(struct576* a1);
 void sub_452190(int a1);
 int* sub_4521A0(int a1);
 int sub_4521F0();
