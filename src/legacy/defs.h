5d4134a0a5c846eb6d3059f920fad49b898a9de9
Jeong, Hun <blmarket@gmail.com>
2025-07-27 11:27:16 -0700

    Add struct264 def

--- src/legacy/defs.h
+++ src/legacy/defs.h
@@ -1368,4 +1368,9 @@ typedef struct struct576 {
 } struct576;
 _Static_assert(sizeof(struct576) == 576, "wrong size of struct576 structure!");
 
+typedef struct struct264 {
+	uint32_t field_0[66];
+} struct264;
+_Static_assert(sizeof(struct264) == 264, "wrong size of struct264 structure!");
+
 #endif // NOX_DEFS_H
