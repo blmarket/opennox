abab174d3b4a5073cf48822e8b654c9de7b28788
Jeong, Hun <blmarket@gmail.com>
2025-08-16 18:56:04 -0700

    Simpler

--- src/legacy/audio/audio_impl.go
+++ src/legacy/audio/audio_impl.go
@@ -597,16 +597,15 @@ func (m *AudioModule) sub_452580(a1 *Struct576) int32 {
 	var v5 int32
 	var ret int32 = 0
 	v1p := a1.field_9
-	v1 := int32(uintptr(unsafe.Pointer(v1p)))
-	if *(*uint32)(unsafe.Pointer(uintptr(v1 + 192))) == 0 {
+	if *(*uint32)(unsafe.Pointer(uintptr(int32(uintptr(unsafe.Pointer(v1p))) + 192))) == 0 {
 		return 0
 	}
 	v3 = int32(a1.field_75)
 	a1.field_109 = 0
-	res = (*Struct312)(unsafe.Pointer(m.sub_452810(int(int32(*(*uint32)(unsafe.Pointer(uintptr(v1 + 48)))+uint32(v3))), 0)))
+	res = (*Struct312)(unsafe.Pointer(m.sub_452810(int(int32(*(*uint32)(unsafe.Pointer(uintptr(int32(uintptr(unsafe.Pointer(v1p))) + 48)))+uint32(v3))), 0)))
 	a1.field_44 = res
 	if res != nil {
-		v4 = int32(m.nox_common_randomIntMinMax_415FF0(int(*(*uint32)(unsafe.Pointer(uintptr(v1 + 76)))), int(*(*uint32)(unsafe.Pointer(uintptr(v1 + 80)))), unsafe.Pointer(alloc.InternCString("C:\\NoxPost\\src\\client\\Audio\\AudEvent.c")), 1482))
+		v4 = int32(m.nox_common_randomIntMinMax_415FF0(int(*(*uint32)(unsafe.Pointer(uintptr(int32(uintptr(unsafe.Pointer(v1p))) + 76)))), int(*(*uint32)(unsafe.Pointer(uintptr(int32(uintptr(unsafe.Pointer(v1p))) + 80)))), unsafe.Pointer(alloc.InternCString("C:\\NoxPost\\src\\client\\Audio\\AudEvent.c")), 1482))
 		a1.field_44.timerGroup_4.Timers[1].SetRaw(uint32(v4 + 100))
 		m.sub_4BDB20(a1.field_44)
 		a1.field_44.field_38 = a1
@@ -615,8 +614,8 @@ func (m *AudioModule) sub_452580(a1 *Struct576) int32 {
 		a1.field_44.field_37 = m.Externs.Sub_4526D0_ptr
 		a1.field_7 = 1
 		a1.field_44.field_28 = &a1.timerGroup_46
-		if int32(*(*uint8)(unsafe.Pointer(uintptr(v1 + 4))))&8 != 0 {
-			v5 = int32(m.nox_common_randomIntMinMax_415FF0(int(*(*uint32)(unsafe.Pointer(uintptr(v1 + 68)))), int(*(*uint32)(unsafe.Pointer(uintptr(v1 + 72)))), unsafe.Pointer(alloc.InternCString("C:\\NoxPost\\src\\client\\Audio\\AudEvent.c")), 1497))
+		if int32(*(*uint8)(unsafe.Pointer(uintptr(int32(uintptr(unsafe.Pointer(v1p))) + 4))))&8 != 0 {
+			v5 = int32(m.nox_common_randomIntMinMax_415FF0(int(*(*uint32)(unsafe.Pointer(uintptr(int32(uintptr(unsafe.Pointer(v1p))) + 68)))), int(*(*uint32)(unsafe.Pointer(uintptr(int32(uintptr(unsafe.Pointer(v1p))) + 72)))), unsafe.Pointer(alloc.InternCString("C:\\NoxPost\\src\\client\\Audio\\AudEvent.c")), 1497))
 			if v5 > 33 {
 				m.sub_452690(a1, int64(v5), 1)
 			}
